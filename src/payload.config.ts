e4a950cd5df050a20b63fd8f8c355de6f7b1b4cb
aniketjha9179 <aniketjha917928@gmail.com>
2023-12-11 10:15:55 +0530

    webpack bundler   for admin dashboard

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,7 @@ import { buildConfig } from "payload/config";
 import {mongooseAdapter} from '@payloadcms/db-mongodb'
 import {slateEditor} from '@payloadcms/richtext-slate'
 import { webpackBundler } from "@payloadcms/bundler-webpack";
+import path from "path";
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
   collections: [],
@@ -10,10 +11,23 @@ export default buildConfig({
   },
   admin:{
     bundler:webpackBundler(),
+    meta:{
+      titleSuffix:'- DigitalHippo',
+      favicon:'/favicon.ico',
+      ogImage:'/thumbnail.jpg',
+    }
+  },
+  rateLimit:{
+    max:2000,
+
   },
   editor:slateEditor({}),
   db:mongooseAdapter({
     url:process.env.MONGODB_URL!,
-  })
+  }),
+  typescript:{
+    outputFile:path.resolve(__dirname, 'payload-types.ts')
+  }
+
   
 });
